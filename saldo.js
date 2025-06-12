e3c6b5f6356adbd130aa907f2b3fec181e6f912c
Eduardo Demétrio <demetrioe153@gmail.com>
2025-06-12 20:29:28 -0300

    Add files via upload

---
+++ saldo.js
@@ -0,0 +1,4 @@
+// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+let saldoDaConta = prompt("Digite o saldo da sua conta: ");
+alert(`O saldo da sua conta é: R$ ${saldoDaConta}`);
